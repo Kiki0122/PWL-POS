5cc9a9ef716d75970dac164b45cf882a8db482f1
Kiki0122 <baihakikiki784@gmail.com>
2025-03-27 20:49:05 +0700

    add ajax route

--- routes/web.php
+++ routes/web.php
@@ -37,9 +37,13 @@ Route::group(['prefix'=>'user'], function(){
     Route::post('/list', [UserController::class, 'list']);
     Route::get('/create', [UserController::class, 'create']);
     Route::post('/', [UserController::class, 'store']);
+    Route::get('/create_ajax', [UserController::class, 'create_ajax']);
+    Route::get('/ajax', [UserController::class, 'store_ajax']);
     Route::get('/{id}', [UserController::class, 'show']);
     Route::get('/{id}/edit', [UserController::class, 'edit']);
     Route::put('/{id}', [UserController::class, 'update']);
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
+    Route::get('/{id}/update_ajax', [UserController::class, 'update_ajax']);
     Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
