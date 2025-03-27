e97b294d4187b7ac2621c348f2b6ba606c7c3623
Kiki0122 <baihakikiki784@gmail.com>
2025-03-27 20:49:48 +0700

    fix index for ajax

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,6 +6,8 @@
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1"> Tambah
+                    Ajax</button>
             </div>
         </div>
         <div class="card-body">
@@ -48,58 +50,66 @@
                     </table>
                 </div>
             </div>
-        @endsection
+@endsection
+        <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data- backdrop="static"
+            data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 
         @push('css')
         @endpush
 
         @push('js')
             <script>
-                $(document).ready(function() {
-                    var dataUser = $('#table_user').DataTable({
+                function modalAction(url = '') {
+                    $('#myModal').load(url, function () {
+                        $('#myModal').modal('show');
+                    });
+                }
+                var dataUser;
+                $(document).ready(function () {
+                    dataUser = $('#table_user').DataTable({
                         serverSide: true,
                         ajax: {
                             url: "{{ url('user/list') }}",
                             dataType: "json",
                             type: "POST",
-                            data: function(d) {
+                            data: function (d) {
                                 d.level_id = $('#level_id').val();
                             }
                         },
                         columns: [{
-                                data: "DT_RowIndex",
-                                className: "text-center",
-                                orderable: false,
-                                searchable: false
-                            },
-                            {
-                                data: "username",
-                                className: "",
-                                orderable: true,
-                                searchable: true
-                            },
-                            {
-                                data: "nama",
-                                className: "",
-                                orderable: true,
-                                searchable: true
-                            },
-                            {
-                                data: "level.level_nama",
-                                className: "",
-                                orderable: false,
-                                searchable: false
-                            },
-                            {
-                                data: "aksi",
-                                className: "",
-                                orderable: false,
-                                searchable: false
-                            }
+                            data: "DT_RowIndex",
+                            className: "text-center",
+                            orderable: false,
+                            searchable: false
+                        },
+                        {
+                            data: "username",
+                            className: "",
+                            orderable: true,
+                            searchable: true
+                        },
+                        {
+                            data: "nama",
+                            className: "",
+                            orderable: true,
+                            searchable: true
+                        },
+                        {
+                            data: "level.level_nama",
+                            className: "",
+                            orderable: false,
+                            searchable: false
+                        },
+                        {
+                            data: "aksi",
+                            className: "",
+                            orderable: false,
+                            searchable: false
+                        }
                         ]
                     });
 
-                    $('#level_id').change(function() {
+                    $('#level_id').change(function () {
                         dataUser.ajax.reload();
                     });
                 });
